ced40ac8e267981cfd601cb5a9cb814c0abdd9fa
David Cai <davidcai2012@gmail.com>
2016-07-29 16:07:27 -0700

    Init commit. Most of CreateAccount is done.
    waiting on  OneSignal integration

---
+++ app/src/main/java/com/example/bike/LoginActivity.java
@@ -0,0 +1,158 @@
+package com.example.bike;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Button mCreateButton;
+    EditText mFirstNameField;
+    EditText mLastNameField;
+    EditText mEmailField;
+    EditText mPasswordField;
+    Spinner mCollegeField;
+    String collegeValue;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mCreateButton = (Button)  findViewById(R.id.createAccountButton);
+        mFirstNameField = (EditText) findViewById(R.id.firstNameField);
+        mLastNameField = (EditText) findViewById(R.id.lastNameField);
+        mEmailField = (EditText) findViewById(R.id.emailField);
+        mPasswordField = (EditText) findViewById(R.id.passwordField);
+        mCollegeField = (Spinner) findViewById(R.id.collegeSpinnerField);
+
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.loginCollegeChoices, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mCollegeField.setAdapter(spinnerAdapter);
+        mCollegeField.setOnItemSelectedListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("BIKE", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("BIKE", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+    }
+
+    public void createFirebaseAccount(View view) {
+        mAuth.createUserWithEmailAndPassword(mEmailField.getText().toString(), mPasswordField.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("BIKE", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            createFBDBEntry();
+                        }
+                    }
+                });
+    }
+
+    public void createFBDBEntry() {
+        // Create userClass object
+        userClass thisUser = new userClass(mFirstNameField.getText().toString(), mLastNameField.getText().toString(), collegeValue, mEmailField.getText().toString());
+
+        // Database Init
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        // First DB entry, users/[user]
+        DatabaseReference userRef = database.getReference("users/" + thisUser.userName);
+        userRef.child("bike").setValue(thisUser.bikeName);
+        userRef.child("college").setValue(thisUser.college);
+        userRef.child("completedwo").child("init").setValue(true); // Hardcoded here b/c not kept locally
+        userRef.child("email").setValue(thisUser.email);
+        userRef.child("name").setValue(thisUser.firstName + thisUser.lastName);
+        userRef.child("oneSignalUserId").setValue(thisUser.oneSignalUserId);
+
+        //Second DB entry, colleges/[college]/users
+        DatabaseReference teamRef = database.getReference("college/" + thisUser.college + "/users/" + thisUser.userName);
+        teamRef.setValue(true);
+
+        //Save thisUser
+        saveUser(thisUser);
+
+    }
+
+    public void saveUser(userClass userToSave) {
+        // Create SharedPreferences instance
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putString("username", userToSave.userName);
+        editor.putString("firstName", userToSave.firstName);
+        editor.putString("lastName", userToSave.lastName);
+        editor.putString("college", userToSave.college);
+        editor.putString("bike", userToSave.bikeName);
+        editor.putString("oneSignalUserId", userToSave.oneSignalUserId);
+
+        Boolean saveStatus = editor.commit();
+        Log.v ("BIKE", "User has been saved: " + saveStatus);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        collegeValue = parent.getItemAtPosition(pos).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // IDK what to do here for now
+    }
+}
---
+++ app/src/main/java/com/example/bike/userClass.java
@@ -0,0 +1,27 @@
+package com.example.bike;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by davidcai on 7/29/16.
+ */
+public class userClass {
+    public String firstName;
+    public String lastName;
+    public String userName;
+    public String college;
+    public String email;
+    public String bikeName;
+    public String oneSignalUserId;
+
+    public userClass(String firstName, String lastName, String college, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.college = college;
+        this.userName = college + firstName + lastName;
+        this.email = email;
+        this.bikeName = "none";
+        this.oneSignalUserId = "asdf";
+    }
+}
